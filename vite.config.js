3990c8f6089b835c3288ae96c3b5fe7381f069e6
Felix Kongyuy <cybrace71@gmail.com>
2026-01-31 22:39:45 -0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path' // You might need to install 'path' if not available
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-});
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // This tells Vite that @ translates to the /src folder
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+})
